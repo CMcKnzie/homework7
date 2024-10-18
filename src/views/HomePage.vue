3e15413aa75bf85f960059f0342cbb4f36f279dc
Caitlyn McKenzie <cmckenzi@stu.southtexascollege.edu>
2024-10-18 11:23:40 -0500

    uploaded homework6 finished state as base

---
+++ src/views/HomePage.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import BaseTitle from '@/components/BaseTitle.vue';
+import useAPI from '@/composables/useAPI'
+import { RouterLink } from 'vue-router';
+
+const { categories, getCategories } = useAPI()
+
+onMounted(async () => {
+  await getCategories()
+})
+
+</script>
+
+<template>
+<BaseTitle>Trivia Application</BaseTitle>
+
+<div class="text-slate-700 grid flex-grow grid-cols-4 gap-12 m-20">
+<RouterLink v-for="category in categories" :key ="category.id"
+:to="`/question/category/${category.id}`" 
+class="bg-pink-100 text-center flex h-32 items-center justify-center rounded-lg border-4 border-pink-300 py-4 font-bold uppercase hover:cursor-pointer hover:border-purple-400 hover:bg-purple-200 hover:text-purple-900 trasition-colors duration-300"
+>
+  {{ category.name }}
+</RouterLink>
+
+  </div>
+</template>
