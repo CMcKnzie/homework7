eb5b64a4eb3005cb3c5ca2b4d9565a328443afa9
Caitlyn McKenzie <cmckenzi@stu.southtexascollege.edu>
2024-10-19 00:00:04 -0500

    displaying answers

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -9,9 +9,23 @@ const api = useAPI()
 const question = ref(null)
 const route = useRoute()
 
+const answers = ref([])
+
 onMounted(async () => {
   question.value = await api.getQuestion(route.params.id)
 
+  answers.value.push({
+    id: answers.value.length, 
+    correct: true, 
+    answer: question.value.correct_answer
+  })
+  question.value.incorrect_answers.map((wrong_answer)=> {
+    answers.value.push({
+      id: answers.value.length, 
+      correct: false, 
+      answer: wrong_answer
+    })
+   })
 } )
 
 </script>
@@ -21,6 +35,9 @@ onMounted(async () => {
 
     <BaseTitle>{{ question.category }} </BaseTitle>
     {{ question.question }}
+<div v-for="answer in answers" v-html="answer.answer" :key="answer.id" class="">
+
+</div>
 
   </div>
   <div v-else class="text-black" >
